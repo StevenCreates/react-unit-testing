e4ade2e912cbf23e78f851113650293656693667
StevenCreates <steven@stevencreates.tech>
2020-11-11 21:01:54 -0700

    added counter test

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ function Counter() {
   const increment = () => setCount((c) => c + 1);
   const decrement = () => setCount((c) => c - 1);
   return (
-    <div>
+    <div id='Counter'>
       <div>Current count: {count}</div>
       <button onClick={decrement}>Decrement</button>
       <button onClick={increment}>Increment</button>
---
+++ src/testing/Counter.test.js
@@ -0,0 +1,17 @@
+import Counter from "../components/Counter";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+
+test("counter increments and decrements when the buttons are clicked", () => {
+  render(<Counter />);
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(<Counter />, div);
+  const [decrement, increment] = div.querySelectorAll("button");
+  const message = div.firstChild.querySelector("div");
+  expect(message.textContent).toBe("Current count: 0");
+  increment.click();
+  expect(message.textContent).toBe("Current count: 1");
+  decrement.click();
+  expect(message.textContent).toBe("Current count: 0");
+});
