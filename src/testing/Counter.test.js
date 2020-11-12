01be6e3ae2da22ed009dfc1457ecdf69220c6f4f
StevenCreates <steven@stevencreates.tech>
2020-11-11 21:07:27 -0700

    added a cleaning up function

--- src/testing/Counter.test.js
+++ src/testing/Counter.test.js
@@ -2,6 +2,10 @@ import Counter from "../components/Counter";
 import ReactDOM from "react-dom";
 import { render } from "@testing-library/react";
 
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
+
 test("counter increments and decrements when the buttons are clicked", () => {
   render(<Counter />);
   const div = document.createElement("div");
@@ -14,4 +18,5 @@ test("counter increments and decrements when the buttons are clicked", () => {
   expect(message.textContent).toBe("Current count: 1");
   decrement.click();
   expect(message.textContent).toBe("Current count: 0");
+  //   div.remove();
 });
