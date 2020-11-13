94c617167932abb6f1eb8ce6328b189f51e5dc5f
StevenCreates <steven@stevencreates.tech>
2020-11-13 08:00:25 -0700

    added submit buton validation for password

--- src/testing/Login.test.js
+++ src/testing/Login.test.js
@@ -6,11 +6,13 @@ test("user and password input accept text and you can change value", () => {
   render(<Login />);
   const username = screen.getByRole("textbox", { name: "Username" });
   const password = screen.getByLabelText("Password");
-
+  const submit = screen.getByRole("button", { name: /submit/i });
   fireEvent.change(username, { target: { value: "Admin" } });
   expect(username.value).toBe("Admin");
   fireEvent.change(username, { target: { value: "$Admin" } });
   expect(username.value).toBe("$Admin");
   fireEvent.change(password, { target: { value: "Password123!" } });
+  expect(password.value).toBe("Password123!");
+  userEvent.click(submit);
   expect(password.value).toBe("Password123!");
 });
