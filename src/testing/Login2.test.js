ef0eb7e0291a11fcf693729130a4e7e59b922db4
StevenCreates <steven@stevencreates.tech>
2020-11-13 13:35:17 -0700

    added another login test

---
+++ src/testing/Login2.test.js
@@ -0,0 +1,18 @@
+import Login2 from "../components/Login2";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+test("user and password input accept text and you can change value", () => {
+  let submittedData;
+  const handleSubmit = (data) => {
+    submittedData = data;
+  };
+  render(<Login2 onSubmit={handleSubmit} />);
+  userEvent.type(screen.getByLabelText(/username/i), "Steven Hofheins");
+  userEvent.type(screen.getByLabelText(/password/i), "Password123");
+  userEvent.click(screen.getByRole("button", { name: /submit/i }));
+  expect(submittedData).toEqual({
+    username: "Steven Hofheins",
+    password: "Password123",
+  });
+});
