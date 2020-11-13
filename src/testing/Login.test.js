dddfb66c92d108100420da3715ba87f904142360
StevenCreates <steven@stevencreates.tech>
2020-11-13 07:54:27 -0700

    added password input to test

--- src/testing/Login.test.js
+++ src/testing/Login.test.js
@@ -5,6 +5,10 @@ import userEvent from "@testing-library/user-event";
 test("validates username and password input", () => {
   render(<Login />);
   const username = screen.getByRole("textbox", { name: "Username" });
+  const password = screen.getByLabelText("Password");
+
   fireEvent.change(username, { target: { value: "Admin" } });
   expect(username.value).toBe("Admin");
+  fireEvent.change(password, { target: { value: "Password123!" } });
+  expect(password.value).toBe("Password123!");
 });
