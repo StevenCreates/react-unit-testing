5fcf0c70d7971c39c07b4605be7c3f835bef2fc0
StevenCreates <steven@stevencreates.tech>
2020-11-13 07:57:03 -0700

    added one more validation to show that you can update the text value

--- src/testing/Login.test.js
+++ src/testing/Login.test.js
@@ -2,13 +2,15 @@ import Login from "../components/Login";
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-test("validates username and password input", () => {
+test("user and password input accept text and you can change value", () => {
   render(<Login />);
   const username = screen.getByRole("textbox", { name: "Username" });
   const password = screen.getByLabelText("Password");
 
   fireEvent.change(username, { target: { value: "Admin" } });
   expect(username.value).toBe("Admin");
+  fireEvent.change(username, { target: { value: "$Admin" } });
+  expect(username.value).toBe("$Admin");
   fireEvent.change(password, { target: { value: "Password123!" } });
   expect(password.value).toBe("Password123!");
 });
